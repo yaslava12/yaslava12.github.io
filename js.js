3065bad37b50b83aaa7651bf940760a96bc44d3f
Slava <slava@slavkin.com>
2023-10-27 16:18:23 +0400

    new kredo

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Дорогу осилит идущий')
+	alert('Дорогу осилит идущий, если он знает git команды')
 })
